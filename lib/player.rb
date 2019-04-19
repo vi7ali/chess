4d8a9e8ea35532fdc58ff986ad675bad519fae62
Vitaly Osipov <vi7ali@gmail.com>
2019-04-19 12:35:51 +0300

    Add get_move method to Player class
    Simple implementation of getting the piece
    and the coordinates to move it to

--- lib/player.rb
+++ lib/player.rb
@@ -5,10 +5,13 @@ class Player
   @@first_color
 
   attr_reader :name, :color
+  attr_accessor :current_move, :current_piece
 
   def initialize(name)
     @name = name
-    @color = generate_color      
+    @color = generate_color
+    @current_piece = nil
+    @current_move = nil
   end  
 
   def generate_color
@@ -18,5 +21,12 @@ class Player
     else
       second_color = @@first_color == "white" ? "black" : "white"
     end
+  end
+
+  def get_move
+    puts "Select your piece"
+    current_piece = gets.chomp!
+    puts "Select your move"
+    current_move = gets.chomp!
   end
 end
