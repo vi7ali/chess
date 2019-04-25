136ba7cae29c389a498a4c14d79028f4bb3a07bf
Vitaly Osipov <vi7ali@gmail.com>
2019-04-25 12:22:03 +0300

    Add board letters and numbers to the board output
    A string of letters and an array of numbers was used to
    added board letters and numbers to the display

--- lib/board.rb
+++ lib/board.rb
@@ -4,9 +4,12 @@ require 'colorize'
 class Board
   LINES = 8
   attr_accessor :board
+  attr_reader :board_letters, :board_numbers
 
   def initialize
     @board = generate_board
+    @board_letters = "  A B C D E F G H"
+    @board_numbers = %w(8 7 6 5 4 3 2 1)
   end
 
   def generate_board
@@ -22,19 +25,24 @@ class Board
   end
 
   def display_board
+    puts board_letters    
+
     (0..board.length-1).each do |row|
       line = ""
-      check = row % 2 == 0 ? 0 : 1
+      switch = row % 2 == 0 ? 0 : 1
       (0..board[row].length-1).each do |square|
-        if square % 2 == (0+check)
+        if square % 2 == (0+switch)
           formatted_square = board[row][square].colorize(:color => :black, :background => :light_white)
         else
           formatted_square = board[row][square].colorize(:color => :black, :background => :white)
         end
-        line.concat(formatted_square)
+        line.concat(formatted_square)        
       end
+      line = board_numbers[row] + " " + line + " " + board_numbers[row]
       puts line
     end
+
+    puts board_letters
   end
 
 end
