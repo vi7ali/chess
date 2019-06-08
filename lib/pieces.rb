62344c6e87b32b16245b669ebbd901b935fc8742
Vitaly Osipov <vi7ali@gmail.com>
2019-06-08 18:29:47 +0300

    Refactor pieces.rb

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,18 +1,26 @@
-#lib/pieces.rb
+# frozen_string_literal: true
 
-require './pieces/pawn'
 require './pieces/rook'
 require './pieces/knight'
 require './pieces/bishop'
 require './pieces/queen'
 require './pieces/king'
+require './pieces/pawn'
 
-class Pieces  
+# lib/pieces.rb
+# Bishop class represents a bishop piece in chess
+# and responds to the public interface 'moves' that
+# returns an array of all of the possible
+# moves(references to cells objects on the board)
+# from the current position provided as an argument
+class Pieces
   attr_reader :all_pieces
 
   def initialize
     @all_pieces = generate_pieces
   end
+
+  private
 
   def generate_piece(piece:, color:)
     piece.new(color)
@@ -21,67 +29,35 @@ class Pieces
   def generate_pieces
     all_pieces = []
     all_pieces = generate_pawns(all_pieces)
-    all_pieces = generate_rooks(all_pieces)
-    all_pieces = generate_knights(all_pieces)
-    all_pieces = generate_bishops(all_pieces)
-    all_pieces = generate_queens(all_pieces)
-    all_pieces = generate_kings(all_pieces)
-    return all_pieces
+    all_pieces = generate_rooks_knights_bishops(all_pieces)
+    all_pieces = generate_queens_kings(all_pieces)
+    all_pieces
   end
 
-  def generate_pawns(all_pieces)    
+  def generate_pawns(all_pieces)
     16.times do |i|
-      color = i < 8 ? "white" : "black"
+      color = i < 8 ? 'white' : 'black'
       all_pieces.push(generate_piece(piece: Pawn, color: color))
     end
-    return all_pieces
+    all_pieces
   end
 
-  def generate_rooks(all_pieces)    
+  def generate_rooks_knights_bishops(all_pieces)
     4.times do |i|
-      color = i < 2 ? "white" : "black"
+      color = i < 2 ? 'white' : 'black'
       all_pieces.push(generate_piece(piece: Rook, color: color))
-    end
-    return all_pieces
-  end
-
-  def generate_knights(all_pieces)    
-    4.times do |i|
-      color = i < 2 ? "white" : "black"
       all_pieces.push(generate_piece(piece: Knight, color: color))
-    end
-    return all_pieces
-  end
-
-  def generate_bishops(all_pieces)    
-    4.times do |i|
-      color = i < 2 ? "white" : "black"
       all_pieces.push(generate_piece(piece: Bishop, color: color))
     end
-    return all_pieces
+    all_pieces
   end
 
-  def generate_queens(all_pieces)    
+  def generate_queens_kings(all_pieces)
     2.times do |i|
-      color = i < 1 ? "white" : "black"
+      color = i < 1 ? 'white' : 'black'
       all_pieces.push(generate_piece(piece: Queen, color: color))
-    end
-    return all_pieces
-  end
-
-  def generate_kings(all_pieces)    
-    2.times do |i|
-      color = i < 1 ? "white" : "black"
       all_pieces.push(generate_piece(piece: King, color: color))
     end
-    return all_pieces
+    all_pieces
   end
-
-  def to_s
-    puts "Pieces"
-    all_pieces.each do |piece|
-      puts "#{piece.name}, #{piece.color}, #{piece.symb}"
-    end
-  end
-
 end
